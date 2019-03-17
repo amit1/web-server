b570c6e9017bc24608fecf10bacd46041816cf0c
amith <amith.mokashi@gmail.com>
2019-03-17 19:35:10 +0530

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+
+// fetch('http://puzzle.mead.io/puzzle').then( (response) => {
+//     response.json().then( (data) => {
+//         console.log(data);
+//     })
+// })
+
+fetch('http://localhost:3000/weather?address=mandya').then( (response) => {
+    response.json().then( (data) => {
+        console.log(data);
+    })
+})
+
+
+const formSubmit = document.querySelector('form');
+const locationVal = document.querySelector('input');
+
+formSubmit.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    fetch('http://localhost:3000/weather?address='+locationVal.value).then( (response) => {
+    response.json().then( (data) => {
+        if(data.error){
+            document.querySelector('.weather-report').innerHTML = data.error;
+        }
+        else{
+            document.querySelector('.weather-report').innerHTML = data[0].forecast;
+        }
+
+    })
+})
+})
